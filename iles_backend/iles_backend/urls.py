a28afd89ad51d3d0b7c02cfdb846ec38463b7013
tamaranazziwa <tamaranazziwa@gmail.com>
2026-05-07 18:57:35 +0300

    debugging 1

--- iles_backend/iles_backend/urls.py
+++ iles_backend/iles_backend/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.token_views import CustomTokenObtainPairView
-
+from django.http import JsonResponse
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),               # /api/users/
@@ -12,4 +13,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')), # browsable API login
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('debug/', lambda request: JsonResponse({
+        'ALLOWED_HOSTS': settings.ALLOWED_HOSTS,
+        'DEBUG': settings.DEBUG,
+        'CORS_ALLOWED_ORIGINS': settings.CORS_ALLOWED_ORIGINS,
+    })),
 ]
